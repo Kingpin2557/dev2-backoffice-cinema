1ac36b03ff4a45efa02a3437a2a3892927082dce
Kingpin2557 <lode.snaet@student.howest.be>
2026-06-01 18:34:58 +0200

    Refactor Postman, add getCustomerById, update movie requests

--- server/controllers/customers/getcustomerbyid.ts
+++ server/controllers/customers/getcustomerbyid.ts
@@ -1,12 +1,11 @@
 import type { Request, Response } from "express";
-
-import { movieById } from "../../services/moviesService";
+import { customerById } from "../../services/customerService";
 
 export const getCustomerById = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
-  const movie = await movieById(req.params.id as string);
+  const movie = await customerById(req.params.id as string);
 
   if (!movie) {
     res.status(404).json({ error: "Movie not found" });
