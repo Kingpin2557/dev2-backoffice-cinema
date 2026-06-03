ac16a50f15a444efebb6c5ccbb48973031e5fdfa
Kingpin2557 <lode.snaet@student.howest.be>
2026-06-03 15:25:42 +0200

    fix: COR permissions

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
+import cors from "cors";
+
 import express from "express";
 import { createServer } from "http";
 import apiRouter from "./routes";
@@ -6,6 +8,12 @@ import apiRouter from "./routes";
 const app = express();
 const httpServer = createServer(app);
 const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // your frontend URL
+  }),
+);
 
 app.use(express.json());
 app.use("/api", apiRouter);
