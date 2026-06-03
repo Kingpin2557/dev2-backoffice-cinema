bd5edc492f97f23d97ecdd5e55f619530908983c
Kingpin2557 <lode.snaet@student.howest.be>
2026-06-03 13:21:00 +0200

    feat: added vercel connection

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ import apiRouter from "./routes";
 
 const app = express();
 const httpServer = createServer(app);
+const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
 
 export const io = new Server(httpServer, {
   cors: { origin: "*" },
@@ -17,7 +18,6 @@ io.on("connection", (socket) => {
 app.use(express.json());
 app.use("/api", apiRouter);
 
-const PORT = 3000;
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
