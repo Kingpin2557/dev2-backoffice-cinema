a89272f063f4c6e7ded40874401ec7dddcd0bfe9
Kingpin2557 <lode.snaet@student.howest.be>
2026-06-05 11:30:16 +0200

    fix: vercel deployment

--- server/services/db.ts
+++ server/services/db.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const postgressUrl: string = String(process.env.POSTGRES_URL);
-const sql: Sql = postgres(postgressUrl);
+const sql: Sql = postgres(postgressUrl, {
+  ssl: process.env.NODE_ENV === "production" ? "require" : false,
+});
 
 export default sql;
